0583776abed444e5e918dea446755a9110a9e959
isa17santos <isacarolina.santos@gmail.com>
2026-04-22 18:54:52 +0100

    PROJ-48 - frontend web gestao recursos humanos

--- backend/routes/users.php
+++ backend/routes/users.php
@@ -10,6 +10,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/users', [UserController::class, 'store']);
     Route::patch('/users/{id}', [UserController::class, 'update']); 
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
     Route::get('/users/{id}/preferences', [NursePreferenceController::class, 'indexByUser']);
 });
 
